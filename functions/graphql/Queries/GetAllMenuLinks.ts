3c21ab49f718b29a88d3a8f36a1441378304045c
Todd Owen Mpeli <toddowenmpeli@rocketmail.com>
2024-06-05 13:55:06 +0100

    Created the sub menu mega menu get all sublinks functions.

--- functions/graphql/Queries/GetAllMenuLinks.ts
+++ functions/graphql/Queries/GetAllMenuLinks.ts
@@ -127,6 +127,231 @@ export const getMegaNavigationUsefulSublinks = async () => {
 	}
 };
 
+// About The Club Sublinks (Mega Navigation)
+export const getAboutTheClubSublinks = async () => {
+	try {
+		const content: any = gql`
+			{
+				aboutTheClubSublinks: menuItems(
+					where: {location: ABOUT_THE_CLUB_SUBLINKS}
+					first: 10
+				) {
+					edges {
+						node {
+							id
+							url
+							label
+						}
+					}
+				}
+			}
+		`;
+
+		const response: any = await client.query({
+			query: content,
+		});
+
+		return response?.data?.aboutTheClubSublinks?.edges;
+	} catch (error: unknown) {
+		console.log(error);
+		throw new Error(
+			"Something went wrong trying to fetch about the club sublinks content"
+		);
+	}
+};
+
+// News Sublinks (Mega Navigation)
+export const getNewsSublinks = async () => {
+	try {
+		const content: any = gql`
+			{
+				newsSublinks: menuItems(where: {location: NEWS_SUBLINKS}, first: 10) {
+					edges {
+						node {
+							id
+							url
+							label
+						}
+					}
+				}
+			}
+		`;
+
+		const response: any = await client.query({
+			query: content,
+		});
+
+		return response?.data?.newsSublinks?.edges;
+	} catch (error: unknown) {
+		console.log(error);
+		throw new Error(
+			"Something went wrong trying to fetch news sublinks content"
+		);
+	}
+};
+
+// Fans Sublinks (Mega Navigation)
+export const getFansSublinks = async () => {
+	try {
+		const content: any = gql`
+			{
+				fansSublinks: menuItems(where: {location: FANS_SUBLINKS}, first: 10) {
+					edges {
+						node {
+							id
+							url
+							label
+						}
+					}
+				}
+			}
+		`;
+
+		const response: any = await client.query({
+			query: content,
+		});
+
+		return response?.data?.fansSublinks?.edges;
+	} catch (error: unknown) {
+		console.log(error);
+		throw new Error(
+			"Something went wrong trying to fetch fans sublinks content"
+		);
+	}
+};
+
+// Community Sublinks (Mega Navigation)
+export const getCommunitySublinks = async () => {
+	try {
+		const content: any = gql`
+			{
+				communitySublinks: menuItems(
+					where: {location: COMMUNITY_SUBLINKS}
+					first: 10
+				) {
+					edges {
+						node {
+							id
+							url
+							label
+						}
+					}
+				}
+			}
+		`;
+
+		const response: any = await client.query({
+			query: content,
+		});
+
+		return response?.data?.communitySublinks?.edges;
+	} catch (error: unknown) {
+		console.log(error);
+		throw new Error(
+			"Something went wrong trying to fetch community sublinks content"
+		);
+	}
+};
+
+// Our History Sublinks (Mega Navigation)
+export const getOurHistorySublinks = async () => {
+	try {
+		const content: any = gql`
+			{
+				ourHistorySublinks: menuItems(
+					where: {location: OUR_HISTORY_SUBLINKS}
+					first: 10
+				) {
+					edges {
+						node {
+							id
+							url
+							label
+						}
+					}
+				}
+			}
+		`;
+
+		const response: any = await client.query({
+			query: content,
+		});
+
+		return response?.data?.ourHistorySublinks?.edges;
+	} catch (error: unknown) {
+		console.log(error);
+		throw new Error(
+			"Something went wrong trying to fetch our history sublinks content"
+		);
+	}
+};
+
+// Benjamin Mkapa Stadium Sublinks (Mega Navigation)
+export const getBenjaminMkapaStadiumSublinks = async () => {
+	try {
+		const content: any = gql`
+			{
+				benjaminMkapaStadiumSublinks: menuItems(
+					where: {location: STADIUM_SUBLINKS}
+					first: 10
+				) {
+					edges {
+						node {
+							id
+							url
+							label
+						}
+					}
+				}
+			}
+		`;
+
+		const response: any = await client.query({
+			query: content,
+		});
+
+		return response?.data?.benjaminMkapaStadiumSublinks?.edges;
+	} catch (error: unknown) {
+		console.log(error);
+		throw new Error(
+			"Something went wrong trying to fetch benjamin mkapa stadium sublinks content"
+		);
+	}
+};
+
+// Partnerships Advertising Sublinks (Mega Navigation)
+export const getPartnershipsAdvertisingSublinks = async () => {
+	try {
+		const content: any = gql`
+			{
+				partnershipsAdvertisingSublinks: menuItems(
+					where: {location: PARTNERSHIPS_ADVERTISING_SUBLINKS}
+					first: 10
+				) {
+					edges {
+						node {
+							id
+							url
+							label
+						}
+					}
+				}
+			}
+		`;
+
+		const response: any = await client.query({
+			query: content,
+		});
+
+		return response?.data?.partnershipsAdvertisingSublinks?.edges;
+	} catch (error: unknown) {
+		console.log(error);
+		throw new Error(
+			"Something went wrong trying to fetch partnerships advertising sublinks content"
+		);
+	}
+};
+
 // Career Sublinks
 export const getCareerSublinks = async () => {
 	try {
