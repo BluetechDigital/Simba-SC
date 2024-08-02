c6b55116c087409c892bd0a12db142dcc0bf49ac
Todd Mpeli <toddowenmpeli@rocketmail.com>
2024-08-02 22:54:25 +0100

    Late night work

--- components/Global/SmoothScrolling.tsx
+++ components/Global/SmoothScrolling.tsx
@@ -3,15 +3,25 @@
 // Imports
 import {FC} from "react";
 import {ReactLenis} from "lenis/react";
+import useWindowSize from "@/Hooks/useWindowSize";
 import {ISmoothScrolling} from "@/types/components/Global";
 
 const SmoothScrolling: FC<ISmoothScrolling> = ({children}) => {
-	return (
-		// <ReactLenis root options={{lerp: 0.01, duration: 0.5, syncTouch: true}}>
-		// 	<main>{children}</main>
-		// </ReactLenis>
+	const {width}: any = useWindowSize();
 
-		<main>{children}</main>
+	return (
+		<ReactLenis
+			root
+			options={{
+				lerp: 0.01,
+				duration: width > 1080 ? 1.75 : 0.5,
+				syncTouch: true,
+			}}
+		>
+			<main>{children}</main>
+		</ReactLenis>
+
+		// <main>{children}</main>
 	);
 };
 
