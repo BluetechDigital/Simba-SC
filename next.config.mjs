f51691f0fa862892bea6e119ec584f2409a85edd
Todd Mpeli <toddowenmpeli@rocketmail.com>
2024-09-19 16:34:18 +0100

    Updated npm packages and content security policies

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	poweredByHeader: false, // Disables the X-Powered-By header
+
 	// All routes edge network caching
 	async headers() {
 		return [
@@ -47,6 +49,16 @@ const nextConfig = {
 						key: "Referrer-Policy",
 						value: "no-referrer",
 					},
+					// Permissions-Policy to restrict features like microphone, geolocation, etc.
+					{
+						key: "Permissions-Policy",
+						value: "geolocation=(), microphone=(), camera=(), payment=()",
+					},
+					// Certificate Transparency (Expect-CT)
+					{
+						key: "Expect-CT",
+						value: "max-age=86400, enforce",
+					},
 				],
 			},
 			{
@@ -66,6 +78,25 @@ const nextConfig = {
 						value: "s-maxage=1, stale-while-revalidate=259200",
 					},
 				],
+			},
+		];
+	},
+	async redirects() {
+		if (process.env.NODE_ENV !== "production") {
+			return [];
+		}
+		return [
+			{
+				source: "/(.*)",
+				has: [
+					{
+						type: "header",
+						key: "x-forwarded-proto",
+						value: "http",
+					},
+				],
+				permanent: true,
+				destination: `${process.env.SITE_URL}/$1`,
 			},
 		];
 	},
