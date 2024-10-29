d7e816829dd9f91c99fa60448b486a01c68fb858
Todd Mpeli <toddowenmpeli@rocketmail.com>
2024-10-29 22:16:49 +0000

    Updated styling fine structure.

--- components/NewsletterBanner.tsx
+++ components/NewsletterBanner.tsx
@@ -5,6 +5,9 @@ import {motion} from "framer-motion";
 import {INewsletterBanner} from "@/types/components";
 import {initialTwo, fadeIn} from "@/animations/animations";
 
+// Styling
+import styles from "@/styles/components/Newsletter.module.scss";
+
 // Components
 import Title from "@/components/Elements/Title";
 import Paragraph from "@/components/Elements/Paragraph";
@@ -19,23 +22,17 @@ const NewsletterBanner: FC<INewsletterBanner> = ({
 	return (
 		<>
 			<div
-				className="py-16 h-full bg-no-repeat bg-cover bg-center"
+				className={styles.newsletterBanner}
 				style={{
 					backgroundImage: `url("/svg/background/low-poly-grid-haikei-red.svg")`,
 				}}
 			>
-				<div className="lg:container px-4 mx-auto">
-					<ScrollYProgressReveal className="w-full flex flex-col items-center justify-center gap-8">
+				<div className={styles.container}>
+					<ScrollYProgressReveal className={styles.content}>
 						<ContentSliceRevealMaskAnimation>
-							<Title
-								content={title}
-								className="max-w-2xl font-schaboCondensed text-center uppercase text-[12.5vw] sm:text-[10vw] md:text-[5vw] tracking-[-0.05rem] text-white leading-tight xl:leading-[5.5rem]"
-							/>
+							<Title content={title} className={styles.title} />
 						</ContentSliceRevealMaskAnimation>
-						<Paragraph
-							content={paragraph}
-							className="max-w-xl mx-auto lg:mx-0 font-OnestRegular leading-tight text-white text-lg text-center lg:text-left"
-						/>
+						<Paragraph content={paragraph} className={styles.paragraph} />
 						<motion.div
 							initial={initialTwo}
 							whileInView={fadeIn}
