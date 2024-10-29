eb777f4e2bb037fa8674cae679e1ea9b10026f2c
Todd Mpeli <toddowenmpeli@rocketmail.com>
2024-10-29 17:44:24 +0000

    Updated styling fine structure.

--- components/Hero/HeroThree.tsx
+++ components/Hero/HeroThree.tsx
@@ -1,8 +1,6 @@
 // Imports
 import {FC} from "react";
-import {motion} from "framer-motion";
 import {IHeroThree} from "@/types/components/index";
-import {fadeIn, initialTwo} from "@/animations/animations";
 
 // Styling
 import styles from "@/styles/components/Hero.module.scss";
@@ -14,31 +12,21 @@ import Paragraph from "@/components/Elements/Paragraph";
 const HeroThree: FC<IHeroThree> = ({title, paragraph, backgroundImage}) => {
 	return (
 		<>
-			<div
-				className={
-					styles.heroThree +
-					` heroThree relative z-50 w-full h-fit pt-[138px] lg:pt-[138px] bg-white`
-				}
-			>
+			<div className={styles.heroThree}>
 				<div
-					className="py-20 w-full h-[50vh] flex flex-col items-center lg:items-baseline justify-center relative bg-primary-dark bg-center bg-no-repeat bg-cover"
+					className={styles.container}
 					style={{
 						backgroundImage: `linear-gradient(0deg,rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),url("${backgroundImage?.sourceUrl}")`,
 					}}
 				>
-					<div className="max-w-full lg:max-w-5xl mx-auto lg:mx-0 relative z-10 flex flex-col items-center lg:items-start px-6 md:px-24">
+					<div className={styles.content}>
 						<Title
 							content={title}
-							className={`title max-w-3xl font-schaboCondensed text-center lg:text-left uppercase  text-7xl sm:text-8xl lg:text-12xl tracking-[-0.05rem] text-white leading-tight xl:leading-[5.5rem] ${
-								paragraph
-									? "border-none"
-									: " pb-2 border-b-[1px] border-solid border-lightGreyTwo"
-							}`}
+							className={
+								styles.title + ` ${paragraph ? styles.after : styles.before}`
+							}
 						/>
-						<Paragraph
-							content={paragraph}
-							className="max-w-2xl mx-auto lg:mx-0 font-OnestRegular leading-tight text-white text-lg text-center lg:text-left"
-						/>
+						<Paragraph content={paragraph} className={styles.paragraph} />
 					</div>
 				</div>
 			</div>
--- components/Hero/HeroTwo.tsx
+++ components/Hero/HeroTwo.tsx
@@ -13,31 +13,23 @@ import Paragraph from "@/components/Elements/Paragraph";
 const HeroTwo: FC<IHeroTwo> = ({title, paragraph, backgroundImage}) => {
 	return (
 		<>
-			<div
-				className={
-					styles.heroTwo +
-					` relative z-50 w-full h-fit pt-[138px] lg:pt-[138px] bg-white`
-				}
-			>
+			<div className={styles.heroTwo}>
 				<div
-					className="py-20 w-full h-[30vh] flex flex-col items-center lg:items-baseline justify-center relative bg-primary-dark bg-center bg-no-repeat bg-cover"
+					className={styles.container}
 					style={{
 						backgroundImage: `linear-gradient(0deg,rgba(234, 29, 37, 0),rgba(234, 29, 37, 0.5),rgba(234, 29, 37, 0.80)),url("${backgroundImage?.sourceUrl}")`,
 					}}
 				>
-					<div className="max-w-full lg:max-w-5xl mx-auto lg:mx-0 relative z-10 flex flex-col items-center lg:items-start px-6 md:px-24">
+					<div className={styles.content}>
 						<motion.h1
 							initial={initialTwo}
 							whileInView={fadeIn}
 							viewport={{once: true}}
-							className="mb-2 font-OnestBlack text-center lg:text-left uppercase text-3xl lg:text-5xl xl:text-7xl tracking-[-0.05rem] text-white font-semibold xl:leading-[3.75rem]"
+							className={styles.title}
 						>
 							{title}
 						</motion.h1>
-						<Paragraph
-							content={paragraph}
-							className="max-w-2xl mx-auto lg:mx-0 font-OnestRegular leading-tight text-white text-lg text-center lg:text-left"
-						/>
+						<Paragraph content={paragraph} className={styles.paragraph} />
 					</div>
 				</div>
 			</div>
