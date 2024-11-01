e6de350b1e8171f031bfd28209f12ec255c518e1
Todd Owen Mpeli <toddowenmpeli@rocketmail.com>
2024-11-01 14:15:43 +0000

    Updated styling.

--- components/TitleParagraph.tsx
+++ components/TitleParagraph.tsx
@@ -5,7 +5,7 @@ import {ITitleParagraph} from "@/types/components";
 import {fadeInUp, initial} from "@/animations/animations";
 
 // Styling
-import styles from "@/styles/components/Elements/Paragraph.module.scss";
+import styles from "@/styles/components/TitleParagraph.module.scss";
 
 // Components
 import Paragraph from "@/components/Elements/Paragraph";
@@ -18,22 +18,18 @@ const TitleParagraph: FC<ITitleParagraph> = ({
 }) => {
 	return (
 		<>
-			<ScrollYProgressReveal className="bg-white lg:container mx-auto flex flex-col py-12 px-8 xl:p-24">
+			<ScrollYProgressReveal className={styles.titleParagraph}>
 				<motion.div
 					initial={initial}
 					whileInView={fadeInUp}
 					viewport={{once: true}}
-					className="py-10 px-4"
+					className={styles.container}
 				>
 					<motion.h2
 						initial={initial}
 						whileInView={fadeInUp}
 						viewport={{once: true}}
-						className={
-							title
-								? "mb-10 text-center font-semibold leading-tight text-4xl lg:text-5xl text-pureBlack"
-								: "hidden"
-						}
+						className={title ? styles.title : "hidden"}
 					>
 						{title}
 					</motion.h2>
@@ -41,8 +37,11 @@ const TitleParagraph: FC<ITitleParagraph> = ({
 						content={paragraph}
 						className={
 							paragraph
-								? `lg:max-w-6xl mx-auto mb-10 font-OnestThin text-pureBlack leading-loose text-paragraph lg:text-medium text-center ${
-										displayParagraph ? "lg:text-center" : "lg:text-left"
+								? styles.paragraph +
+								  ` ${
+										displayParagraph
+											? "text-center lg:text-center"
+											: "text-center lg:text-left"
 								  }`
 								: "hidden"
 						}
